9d51b40ff467f65d0be03e22e09cc671431c4a26
Jessica White <jessica.jm.white@gmail.com>
2019-08-14 19:33:31 -0400

    style navigation and position svg

--- src/entry.js
+++ src/entry.js
@@ -1,27 +1,34 @@
 import { returnData } from './data/data';
 
-const allData = returnData();
-
 // Set up the display area
+const margin = { top: 20, right: 50, bottom: 20, left: 50 };
+const width = window.innerWidth - margin.right - margin.left;
+const height = window.innerHeight - margin.top - margin.bottom;
+
 const displayArea = d3.select("main")
                       .append("svg")
-                      .attr("width", 1000)
-                      .attr("height", 1000)
-                      .append("g").attr("transform", "translate(250,100)")
+                      .attr("width", width + margin.right + margin.left)
+                      .attr("height", height + margin.top + margin.bottom)
+                      .append("g")
+                      .attr("transform", `translate(50, ${height / 2})`)
 
+// set up the data structure
+const allData = returnData();
 const hierarchicalData = d3.stratify()
                            .id( (d) => {return d.id} )
                            .parentId( (d) => {return d.parentId} )
                            (allData);
 
-const tree = d3.tree().size([400, 400])
-  // .separation((a, b) => { return (a.parent === b.parent ? 5 : 10) });
+const  tree = (data) => {
+  data.dx = 30;
+  data.dy = width / (data.height + 1);
+  return d3.tree().nodeSize([data.dx, data.dy])(data);
+}
 
 const dataTree = tree(hierarchicalData);
 
 // TESTING//
   console.log(dataTree.descendants());
-  console.log(dataTree.links());
 ////////////
 
 // Add nodes for each descendant in the tree
@@ -31,7 +38,7 @@ const circles = displayArea.append("g").selectAll("circle")
 circles.enter().append("circle")
              .attr("cx", d => { return d.y })
              .attr("cy", d => { return d.x })
-             .attr("r", 5);
+             .attr("r", 3);
 
 // Add paths between each node
 const connections = displayArea.append("g").selectAll("path")
@@ -42,10 +49,11 @@ connections.enter().append("path")
                    .x(d => d.y)
                    .y(d => d.x));
 
+// Add words and position relative to nodes
 const words = displayArea.append("g").selectAll("text")
                          .data(dataTree.descendants());
 
 words.enter().append("text")
              .text( d => { return d.data.word })
-             .attr("x", d => { return d.y-5 })
-             .attr("y", d => { return d.x-10 })
+             .attr("x", d => { return d.y - 5 })
+             .attr("y", d => { return d.x - 10 })
