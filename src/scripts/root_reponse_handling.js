174f65d0b3ce590784207dfdf86a32136e0be8a6
Jessica White <jessica.jm.white@gmail.com>
2019-08-28 10:09:08 -0400

    refactor objects in root response handling

--- src/scripts/root_reponse_handling.js
+++ src/scripts/root_reponse_handling.js
@@ -1,33 +1,35 @@
 import { idGenerator } from './node_utilities'
-import d3Display from '../d3/d3';
 import { createErrorNode } from './create_error_node';
+import d3Display from '../d3/d3';
 
 export const handleRootResponse = (jsonResponse) => {
   // this is an array of one object: the root word
   const data = JSON.parse(sessionStorage.getItem('data'))
   const root = data[0];
   if (jsonResponse[0] instanceof Object) {
-    jsonResponse.forEach( type => {
+    jsonResponse.forEach((type, idx) => {
       if (type.hwi.hw === root.word) {
-        let rootChildObj = {};
-        rootChildObj['id'] = idGenerator();
-        rootChildObj['parentId'] = '_1';
-        rootChildObj['wordType'] = type.fl;
-        rootChildObj['word'] = type.fl;
-        rootChildObj['def'] = type.shortdef[0];
+        let rootChildObj = {
+          id: idGenerator(),
+          parentId: "_1",
+          wordType: type.fl,
+          word: type.fl,
+          def: type.shortdef
+        };
         data.push(rootChildObj)
 
         let syns = type.meta.syns[0];
         while (syns.length) {
-          let childNode = {};
-          childNode['id'] = idGenerator();
-          childNode['parentId'] = rootChildObj.id;
-          childNode['wordType'] = rootChildObj.wordType;
-          childNode['def'] = 'click to see definition and synonyms';
-          childNode['word'] = syns.shift();
+          let childNode = {
+            id: idGenerator(),
+            parentId: rootChildObj.id,
+            wordType: rootChildObj.wordType,
+            word: syns.pop(),
+            def: 'click to see definition and synonyms'
+          };
           data.push(childNode)
         }
-      } else if ((type.hwi.hw !== root.word && jsonResponse.length === 1)) {
+      } else if ((type.hwi.hw !== root.word && idx === 0)) {
         let errorNode = createErrorNode(root.word);
         data.push(errorNode);
       }
@@ -36,9 +38,7 @@ export const handleRootResponse = (jsonResponse) => {
     let errorNode = createErrorNode(root.word);
     data.push(errorNode);
   }
-  
+
   sessionStorage.setItem('data', JSON.stringify(data))
-
   d3Display(data);
-
 }
